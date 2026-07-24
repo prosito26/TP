0cfcab8edc50e73440d42e5a455bdfe55e6584b6
prosito26 <a20231476@pucp.edu.pe>
2026-07-24 17:46:07 -0500

    Update main.cpp

--- Labs/2025-2/Lab9/PropuestaSolucion/main.cpp
+++ Labs/2025-2/Lab9/PropuestaSolucion/main.cpp
@@ -1,10 +1,5 @@
 #include "Bibliotecas/FuncionesAuxiliares.h"
-/*
- * NOMBRE: ALVARO HUARI
- * CODIGO: 20231476
-    DESCRIPCION:Mi programa usa el paradigma de las esturcturas para guardar una categoria con su codigo,nombre y descipcion; así mismo
-    cada categoria se reproduce en un canal con su respectivo rating y duracion,además, se agrega la lista de comentarios.
- */
+
 int main() {
     struct Categoria *arrCategorias;
     int cantidadCategorias = 0;
